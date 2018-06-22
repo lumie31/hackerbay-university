db3680be5ab8f6aeb85d7011a58572b2bbccd88c
Olumide <odokedusi@sidmach.com>
2018-06-22 14:44:45 +0100

    Fix issues with server.js file

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.status(200).send("success"));
-app.post("/data", (req, res, next) => {
-  let data = req.body.data;
-  res.json(data);
-  next();
+
+let data = "Any string";
+
+app.post("/data", (req, res) => {
+  return res.status(200).json(data);
 });
 app.get("/data", (req, res) => {
-  res.json({ data: req.data });
+  res.json({ data });
 });
 app.listen(port, () => console.log(`Server started on port ${port}`));
