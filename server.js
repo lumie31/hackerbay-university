5abdb2a4707321f8828c32a4f1f3fb0d8334666f
Olumide <odokedusi@sidmach.com>
2018-06-25 05:25:24 +0100

    Modified .gitignore and server.js files

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.status(200).send("success"));
 
-let data = "Any string";
+let data;
 
 app.post("/data", (req, res) => {
+  data = req.body.data;
   return res.status(200).json(data);
 });
 app.get("/data", (req, res) => {
-  res.json({ data });
+  return res.json(data);
 });
 app.listen(port, () => console.log(`Server started on port ${port}`));
+ 
