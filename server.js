21dacaa217f72e1b36f3a922b68e9ae58be48664
Olumide <odokedusi@sidmach.com>
2018-06-30 19:00:27 +0100

    Update package.json + changes to server.js file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Sequelize = require('sequelize');
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -20,4 +21,32 @@ app.get("/data", (req, res) => {
   return res.json(data);
 });
 app.listen(port, () => console.log(`Server started on port ${port}`));
- 
+
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'path/to/database.sqlite'
+});
+
+// Or you can simply use a connection uri
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
