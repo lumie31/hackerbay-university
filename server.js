dd076f687cb21c29fd1a3da6d45f95f5070a4377
Olumide <odokedusi@sidmach.com>
2018-07-03 10:08:05 +0100

    Implemented braching for task-2 + Fix issues with passport and JWT

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const passportLocalSequelize = require("passport-local-sequelize");
 const session = require("express-session");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -38,26 +38,6 @@ app.use(passport.session());
 //   return res.json(data);
 // });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
-//Passport local for Auth
-passport.use(
-  new LocalStrategy(function(username, password, done) {
-    User.findOne({ username: email }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username." });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-      return done(null, user);
-    });
-  })
-);
-
 // Add postgres connection and db logic
 const connection = new Sequelize("hack", "postgres", "lumidizzle31", {
   host: "localhost",
@@ -79,7 +59,6 @@ let User = connection.define("user", {
     unique: true,
     validate: {
       isEmail: true,
-      isLowercase: true,
       notEmpty: true
     }
   },
@@ -97,14 +76,12 @@ app.post("/user/signup", (req, res) => {
       message: "Incorrect details"
     });
   }
-
   if (email && password) {
     User.beforeValidate((user, options) => {
       return bcrypt.hash(user.password, saltRounds).then(hashedPw => {
         user.password = hashedPw;
       });
     });
-
     connection.sync().then(function() {
       User.findOrCreate({
         where: {
@@ -117,9 +94,56 @@ app.post("/user/signup", (req, res) => {
             success: true,
             message: "User created successfully"
           });
+        } else {
+          return res.status(400).json({
+            message: false,
+            message: "User already exists"
+          });
         }
       });
     });
   }
-  // console.log(req.body);
 });
+
+//Passport local for Auth
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email", passwordField: "password" },
+    function(email, password, done) {
+      return User.findOne({ email, password })
+        .then(user => {
+          if (!user) {
+            return done(null, false, {
+              message: "Incorrect email or password"
+            });
+          }
+          return done(null, user, { message: "Logged In Successfully" });
+        })
+        .catch(err => done(err));
+    }
+  )
+);
+
+app.post("/user/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: "Something is not right",
+        user
+      });
+    }
+    req.login(user, err => {
+      if (err) {
+        return res.send(err);
+      }
+      const token = jwt.sign(user, "secrettoken123");
+      return res.json({
+        success: true,
+        token: "JWT" + token,
+        user
+      });
+    });
+  })(req, res);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
